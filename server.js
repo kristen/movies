552b839d9586a29e1c963f58139cc7edd853c2a1
Kristen Sundquist <kssundquist@gmail.com>
2019-10-15 05:43:19 -0700

    Add node server to make api calls to the movie db

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
+
+const port = 3001;
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/status', (req, res) => {
+    return res.send('OK');
+});
+
+const apiKey = '?api_key=75b9387059d3bb6140f868f1a4c771ac';
+const baseUrl = 'https://api.themoviedb.org/3';
+
+app.get('/movies/popular', (req, res, next) => {
+    fetch(`${baseUrl}/movie/popular${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+app.get('/search/movie', (req, res, next) => {
+    fetch(`${baseUrl}/search/movie${apiKey}&query=${req.query.search}`)
+        .then(res => res.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+app.get('/movie/:movieId', (req, res, next) => {
+    console.log('params', req.params);
+    fetch(`${baseUrl}/movie/${req.params.movieId}/images${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
